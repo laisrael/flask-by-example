16c62c44f67c1e20d75f786eb8e3c1972162cbce
laisrael <laisrael@reed.edu>
2016-05-19 13:14:05 -0700

    asdalk

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import os
 from flask import Flask
+
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
 
 @app.route('/')
 def hello():
-	return "Hi, bunny!"
+    return "Hello World!"
+
 
 @app.route('/<name>')
 def hello_name(name):
-	return "Hello {}!".format(name)
+    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
